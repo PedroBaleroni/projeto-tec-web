889e05cf535c3f0dd8dafe0ea598b5e1089516d7
Pedro Baleroni <pbaleroni@gmail.com>
2025-10-30 19:34:47 -0300

    criação models

---
+++ app/Http/Controllers/QuadraController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Quadra;
+use Illuminate\Http\Request;
+
+class QuadraController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Quadra $quadra)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Quadra $quadra)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Quadra $quadra)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Quadra $quadra)
+    {
+        //
+    }
+}
